71e5e9e74a9d688a2707046e9bd914a505d74f3f
Lucas Eduardo <lucaseduardo3528@hotmail.com>
2024-11-16 21:54:46 -0300

    test: implementing test_register

--- tests/Unit/AuthTest.php
+++ tests/Unit/AuthTest.php
@@ -44,10 +44,24 @@ class AuthTest extends TestCase
         $this->assertGuest();
     }
 
-    public function test_empty_fields(): void
+    public function test_authentication_with_empty_fields(): void
     {
         $response = $this->post('/login', []);
 
         $response->assertStatus(302);
+    }
+
+    public function test_register(): void
+    {
+        $response = $this->post('/register', [
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => Hash::make('password123')
+        ]);
+
+        $user = User::query()->where('email', 'test@example.com')->exists();
+
+        $response->assertStatus(200);
+        $this->assertTrue($user);
     }
 }
