e03b6e564ec36809c313a9c0e1f2cdc20c114ef4
Lucas Eduardo <lucaseduardo3528@hotmail.com>
2024-11-19 22:27:05 -0300

    refactor: refactoring actions buttons]

--- resources/js/Layouts/TasksTab.vue
+++ resources/js/Layouts/TasksTab.vue
@@ -120,7 +120,7 @@ const filteredTasks = computed(() => {
             </select>
         </div>
 
-        <div class="overflow-y-auto max-h-[700px]">
+        <div class="overflow-y-auto max-h-[550px]">
 
             <table class="w-full text-left border-collapse border border-[#697076] max-w-6xl max-h-[90%] overflow-auto">
                 <thead class="bg-[#2f2c2c] text-white">
@@ -137,7 +137,9 @@ const filteredTasks = computed(() => {
                         Status
                         <span v-if="sortKey === 'status'" class="ml-2">{{ sortOrder === 'asc' ? '↑' : '↓' }}</span>
                     </th>
-                    <th class="p-4 border border-[#697076] text-center">Ações</th>
+                    <th class="p-4 border border-[#697076] text-center h-auto">
+                        Ações
+                    </th>
                 </tr>
                 </thead>
                 <tbody>
@@ -154,25 +156,36 @@ const filteredTasks = computed(() => {
                     <td class="p-4 border border-[#697076]">{{ task.title }}</td>
                     <td class="p-4 border border-[#697076]">{{ task.description }}</td>
                     <td class="p-4 border border-[#697076]">{{ task.status }}</td>
-                    <td class="p-4 border border-[#697076] text-center">
-                        <button
-                            @click="editTask(task)"
-                            class="bg-blue-600 text-white px-3 py-2 rounded hover:bg-blue-800 transition-colors mr-2"
-                        >
-                            Editar
-                        </button>
-                        <button
-                            @click="toggleStatus(task)"
-                            class="bg-green-600 text-white px-3 py-2 rounded hover:bg-green-800 transition-colors mr-2"
-                        >
-                            {{ task.status === "Pendente" ? "Concluir" : "Reabrir" }}
-                        </button>
-                        <button
-                            @click="deleteTask(task.id)"
-                            class="bg-red-600 text-white px-3 py-2 rounded hover:bg-red-800 transition-colors"
-                        >
-                            Remover
-                        </button>
+                    <td class="p-4 border border-[#697076]">
+                        <div class="flex items-center h-full">
+
+                            <button
+                                @click="editTask(task)"
+                                class="bg-blue-600 text-white px-1 py-1 rounded hover:bg-blue-800 transition-colors mr-2"
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                </svg>
+
+                            </button>
+                            <button
+                                @click="toggleStatus(task)"
+                                class="bg-green-600 text-white px-1 py-1 rounded hover:bg-green-800 transition-colors mr-2"
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+                                </svg>
+                            </button>
+                            <button
+                                @click="deleteTask(task.id)"
+                                class="bg-red-600 text-white px-1 py-1 rounded hover:bg-red-800 transition-colors"
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5m6 4.125 2.25 2.25m0 0 2.25 2.25M12 13.875l2.25-2.25M12 13.875l-2.25 2.25M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
+                                </svg>
+
+                            </button>
+                        </div>
                     </td>
                 </tr>
                 </tbody>
